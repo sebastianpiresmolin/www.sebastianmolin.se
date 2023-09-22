6b47e818129d7aa0ca148658bdc5da2dd9bb6438
sebastianpiresmolin <piresmolinsecure@gmail.com>
2023-09-22 12:30:24 +0200

    install react for timeline app

---
+++ main.js
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);

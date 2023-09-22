05dcc87165d0da2f088df5a5b0a785df100354af
sebastianpiresmolin <piresmolinsecure@gmail.com>
2023-09-22 22:22:57 +0200

    trying to set up react

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-import { createRoot } from 'react-dom/client';
-
-
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+cd

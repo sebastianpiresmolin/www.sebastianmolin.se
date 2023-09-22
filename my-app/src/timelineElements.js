7bd9469159e87b60997dd85a7f10c7ef167802ea
sebastianpiresmolin <piresmolinsecure@gmail.com>
2023-09-22 16:46:53 +0200

    start adding data in timelineElement.js

---
+++ my-app/src/timelineElements.js
@@ -0,0 +1,29 @@
+let timelineElements = [
+    {
+        id: 1,
+        title: "Fullstack Developer Student",
+        location: "Lernia Tradeschool, Remote",
+        description: "Systems developer Java/JS - Fullstack. Studying at full pace and work at my last job for 1 week a month",
+        buttonText: "Read about the curriculum",
+        date: "September 2023 - present",
+        icon: "school",
+    },
+    {
+        id: 2,
+        title: "Machine Operator",
+        location: "Aristo AB, Falkoping",
+        description: "Responsible for the maintenance and production of a paper bag manufacturing machine. Work Description include forklift operations and night shifts",
+        buttonText: "View Aristo AB Contacts",
+        date: "September 2018 - present",
+        icon: "work",
+    },
+    {
+        id: 3,
+        title: "Group Leader, Logistics Soldier",
+        location: "Swedish Armed Forces, Skovde",
+        description: "Responsible for a group of 8 soldiers, planning and executing strategic combat and logistical operations.",
+        buttonText: "View Detailed Work Description",
+        date: "May 2017 - August 2018",
+        icon: "Work",
+    },
+]
